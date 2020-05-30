e866b539acbda620b14641c549592c67e538d4fd
Isabelle Levent <olivialevent@gmail.com>
2020-05-29 22:25:15 -0400

    Iteration 0

---
+++ toDo/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  toDo
+//
+//  Created by Isabelle Levent on 5/29/20.
+//  Copyright © 2020 Isabelle Levent. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false 
+}
